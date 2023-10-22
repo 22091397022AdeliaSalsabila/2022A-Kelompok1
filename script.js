a6b91c76a5867eb6cb5f56e3d1afb103ed375095
Adelia Salsabila <124540149+22091397022AdeliaSalsabila@users.noreply.github.com>
2023-10-23 03:53:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const API_KEY = "165b7f43e35f489f9fb0385aeba7bed3"
 const url = "https://newsapi.org/v2/everything?q="
 
 
-
 async function fetchData(query){
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`)
     const data = await res.json()
     return data
 }
+
 fetchData("all").then(data => renderMain(data.articles))
 
 //menu button
-let mobilemenu = document.querySelector(".mobile")
+/*let mobilemenu = document.querySelector(".mobile")
 let menuButton = document.querySelector(".menuButton")
 let menuButtonDisplay = true;
 
 menuButton.addEventListener("click",()=>{
     mobilemenu.classList.toggle("hidden")
-})
+})*/
 
 
 //render news 
@@ -27,7 +27,7 @@ function renderMain(arr){
         if(arr[i].urlToImage){
         mainHTML += ` <div class="card">
                         <a href=${arr[i].url}>
-                        <img src=${arr[i].urlToImage} lazy="loading" />
+                        <img src=${arr[i].urlToImage} lazy="loading"/>
                         <h4>${arr[i].title}</h4>
                         <div class="publishbyDate">
                             <p>${arr[i].source.name}</p>
@@ -38,18 +38,16 @@ function renderMain(arr){
                            ${arr[i].description}
                         </div>
                         </a>
-                     </div>
-        `;
+                     </div>`;
         }
     }
 
     document.querySelector("main").innerHTML = mainHTML
 }
 
-
 const searchButton = document.getElementById("searchForm")
-const searchButtonMobile = document.getElementById("searchFormMobile")
-const searchInputMobile = document.getElementById("searchInputMobile") 
+/*const searchButtonMobile = document.getElementById("searchFormMobile")
+const searchInputMobile = document.getElementById("searchInputMobile")*/ 
 const searchInput = document.getElementById("searchInput")
 
 searchButton.addEventListener("submit",async(e)=>{
@@ -58,13 +56,13 @@ searchButton.addEventListener("submit",async(e)=>{
 
     const data = await fetchData(searchInput.value)
     renderMain(data.articles)
-
 })
-searchButtonMobile.addEventListener("submit",async(e)=>{
+
+/*searchButtonMobile.addEventListener("submit",async(e)=>{
     e.preventDefault()
     const data = await fetchData(searchInputMobile.value)
     renderMain(data.articles)
-})
+})*/
 
 
 async function Search(query){
